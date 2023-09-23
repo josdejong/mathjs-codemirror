b34ad5461ec6d8bb6fe4028063363d9d3f1d5a9c
David Contreras <dvd.cnt@gmail.com>
2023-09-23 19:06:14 +0000

    Completions proof of concept

--- src/mathjs-lang.js
+++ src/mathjs-lang.js
@@ -180,7 +180,22 @@ export function mathjsLang(math) {
     },
 
     languageData: {
-      commentTokens: { line: '#' }
+      commentTokens: { line: '#' },
+      autocomplete: myCompletions
     }
+  }
+}
+
+function myCompletions(context) {
+  let word = context.matchBefore(/\w*/)
+  if (word.from == word.to && !context.explicit)
+    return null
+  return {
+    from: word.from,
+    options: [
+      {label: "match", type: "keyword"},
+      {label: "hello", type: "variable", info: "(World)"},
+      {label: "magic", type: "text", apply: "⠁⭒*.✩.*⭒⠁", detail: "macro"}
+    ]
   }
 }
